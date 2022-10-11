94af8571e320c878a1d6ab2cc4dbdb813039b5cc
kyungho <kyungho_kim@mycelebs.com>
2022-10-11 18:22:51 +0900

    [develop] 로그(Timber) 제거

--- app/src/main/java/com/seno/game/MainActivity.kt
+++ app/src/main/java/com/seno/game/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.seno.game
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,25 +12,21 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import com.google.firebase.auth.FirebaseAuth
-import com.seno.game.extensions.screenHeight
-import com.seno.game.extensions.screenWidth
 import com.seno.game.extensions.startActivity
 import com.seno.game.theme.AppTheme
 import com.seno.game.ui.game.areagame.AreaGameActivity
 import com.seno.game.ui.game.diffgame.DiffPictureGameActivity
 import com.seno.game.ui.game.humminjeongeumgame.HunMinJeongEumActivity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel>()
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setAuthentication() {
+        setAuthentication {
             if (it) {
                 setContent {
                     AppTheme {
@@ -86,7 +80,6 @@ class MainActivity : AppCompatActivity() {
         if (auth.currentUser != null) {
             callback(true)
         } else {
-            Timber.e("kkh not user ")
             auth.signInAnonymously()
                 .addOnSuccessListener { callback(true) }
                 .addOnFailureListener { callback(false) }

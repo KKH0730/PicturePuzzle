ca8dff34fde742157fae1559a935bf99b1dfd49b
김경호 <kh.kim@mediaweb.co.kr>
2025-12-30 15:20:52 +0900

    [feature] splash // splash 배경 수정

--- app/src/main/java/com/seno/game/ui/main/home/screen/HomeLoadingScreen.kt
+++ app/src/main/java/com/seno/game/ui/main/home/screen/HomeLoadingScreen.kt
@@ -1,12 +1,16 @@
 package com.seno.game.ui.main.home.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,24 +18,31 @@ import com.seno.game.R
 
 @Composable
 fun HomeLoadingScreen() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = colorResource(id = R.color.color_e7c6ff))
-    ) {
-        Spacer(modifier = Modifier.height(height = 210.dp))
+    Box(modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.ic_splash),
+            painter = painterResource(id = R.drawable.ic_home_background),
             contentDescription = null,
-            modifier = Modifier.width(width = 216.dp)
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.weight(weight = 1f))
-        Image(
-            painter = painterResource(id = R.drawable.ic_studio_seno_logo),
-            contentDescription = null,
-        )
-        Spacer(modifier = Modifier.height(height = 36.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(height = 210.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_splash),
+                contentDescription = null,
+                modifier = Modifier.width(width = 216.dp)
+            )
+            Spacer(modifier = Modifier.weight(weight = 1f))
+            Image(
+                painter = painterResource(id = R.drawable.ic_studio_seno_logo),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(height = 36.dp))
+        }
     }
 }
 
--- app/src/main/java/com/seno/game/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/seno/game/ui/splash/SplashScreen.kt
@@ -1,12 +1,22 @@
 package com.seno.game.ui.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.seno.game.R
@@ -16,7 +26,7 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     onFinished: () -> Unit
 ) {
-    var countState by remember { mutableStateOf(0) }
+    var countState by remember { mutableIntStateOf(0) }
     LaunchedEffect(countState) {
         if (countState > 2) {
             onFinished.invoke()
@@ -25,24 +35,30 @@ fun SplashScreen(
         delay(1000)
         countState += 1
     }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = colorResource(id = R.color.color_e7c6ff))
-    ) {
-        Spacer(modifier = Modifier.height(height = 210.dp))
+    Box(modifier = Modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.ic_splash),
+            painter = painterResource(id = R.drawable.ic_home_background),
             contentDescription = null,
-            modifier = Modifier.width(width = 216.dp)
-                .aspectRatio(ratio = 2.37f)
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.weight(weight = 1f))
-        Image(
-            painter = painterResource(id = R.drawable.ic_studio_seno_logo),
-            contentDescription = null,
-        )
-        Spacer(modifier = Modifier.height(height = 36.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(height = 210.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_splash),
+                contentDescription = null,
+                modifier = Modifier.width(width = 216.dp)
+                    .aspectRatio(ratio = 2.37f)
+            )
+            Spacer(modifier = Modifier.weight(weight = 1f))
+            Image(
+                painter = painterResource(id = R.drawable.ic_studio_seno_logo),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(height = 36.dp))
+        }
     }
 }
